4b9601ce836c05a73866fbf87a204a80acd77a2d
Pawel Dabrowa <paweldabrowa@Pawels-MacBook-Air.local>
2019-05-05 22:28:52 +0100

    add function clickElement

--- main.js
+++ main.js
@@ -9,10 +9,27 @@ document.querySelector('body').appendChild(h1Text);
 const clickElement = document.querySelector('body');
 const textCenter = document.querySelector('h1');
 
+const pText = document.createElement('p');
+document.querySelector('body').appendChild(pText);
 
+textCenter.textContent = 'Click';
 clickElement.addEventListener('click', function (e) {
-    console.log('dziala');
-    textCenter.textContent = e.clientX + " | " + e.clientY;
+    const paragraphCenter = document.querySelector('p');
+    let x = e.clientX;
+    let y = e.clientY;
 
+    console.log('dziala');
+    textCenter.textContent = x + " | " + y;
+
+    if ((x % 2) !== 1 && (y % 2) !== 1) {
+        clickElement.style.backgroundColor = 'red';
+        paragraphCenter.textContent = 'if both positions are even, body is red';
+    } else if ((x % 2) !== 0 && (y % 2) !== 0) {
+        clickElement.style.backgroundColor = 'blue';
+        paragraphCenter.textContent = 'if both positions are odd body is blue';
+    } else {
+        clickElement.style.backgroundColor = 'green';
+        paragraphCenter.textContent = 'if positions are odd and even body is green';
+    }
 });
 
